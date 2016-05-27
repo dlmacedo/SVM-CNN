16cb32e392a0cfce75336ebaa29354d2214e0f7e
dlmacedo <dlm@cin.ufpe.br>
2016-05-27 03:10:12 -0300

    Commint for the first time fromconsole...

--- main.py
+++ main.py
@@ -13,24 +13,6 @@ epoch_size = 1000
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-"""
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-sess.run(tf.initialize_all_variables())
-
-y = tf.nn.softmax(tf.matmul(x,W) + b)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-for i in range(20000):
-    batch = mnist.train.next_batch(55)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-"""
-
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
